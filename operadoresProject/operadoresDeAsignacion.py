5def0155a57a27397117b4cc6086c33a90ffce1e
anderson <andersonllanten09@gmail.com>
2024-09-26 16:49:11 -0500

    se crea el proyecto con los ejemplos de los operadores

---
+++ operadoresProject/operadoresDeAsignacion.py
@@ -0,0 +1,26 @@
+#ejemplos de operadores de asignacon
+var1 = 5
+var2 = 12
+var3 = 8
+var4 = 14
+var5 = 6
+var6 = 8
+
+var1 += 3 # var1 = var + 3 asignacion con suma
+print(f"asignacion con suma: {var1}")
+
+var2 -= 3 # var2 = var - 3 asignacion con resta
+print(f"asignacion con resta: {var2}")
+
+var3 *= 3 # var3 = var * 3 asignacion con multiplicacion
+print(f"asignacion con multiplicacion: {var3}")
+
+var4 /= 3 # var4 = var / 3 asignacion con division
+print(f"asignacion con division: {var4}")
+
+var5 %= 3 # var5 = var + 3 asignacion con residuo
+print(f"asignacion con residuo: {var5}")
+
+var6 //= 3 # var6 = var // 3 asignacion con division entera
+print(f"asignacion con divisionentera: {var6}")
+
